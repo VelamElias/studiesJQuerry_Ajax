aa912feaadac59de2453602bd5dba0dd5ce26f0f
VelamElias <elnatasource@gmail.com>
2020-06-19 16:04:52 -0300

    Consulta de CEP com classes em JS e solicitação com Ajax

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,22 @@
-alert("Running");
+//alert("Running");
+
+function consultaCEP()
+{
+    var cep = document.getElementById("CEP").value;
+    var url = "https://viacep.com.br/ws/" + cep +"/json/";
+
+    //requisição ajax usando biblioteca jquerry
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function(response){
+            document.getElementById("logradouro").innerHTML = response.logradouro;
+            document.getElementById("bairro").innerHTML = response.bairro;
+            document.getElementById("localidade").innerHTML = response.localidade;
+            document.getElementById("uf").innerHTML = response.uf;
+
+            //Usando AJAX puro para consultar a UF
+            $("#ufAJ").html(response.uf);
+        }
+    })
+}
