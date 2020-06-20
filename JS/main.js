dd620b89196d4ea5f184429bf93459cb10f56983
VelamElias <elnatasource@gmail.com>
2020-06-20 15:07:39 -0300

    include research of address information with ajax and create table to show it

--- JS/main.js
+++ JS/main.js
@@ -2,21 +2,27 @@
 
 function consultaCEP()
 {
-    var cep = document.getElementById("CEP").value;
+    var cep = document.getElementById("cep").value;
     var url = "https://viacep.com.br/ws/" + cep +"/json/";
-
+    
     //requisição ajax usando biblioteca jquerry
     $.ajax({
         url: url,
         type: "GET",
         success: function(response){
-            document.getElementById("logradouro").innerHTML = response.logradouro;
-            document.getElementById("bairro").innerHTML = response.bairro;
-            document.getElementById("localidade").innerHTML = response.localidade;
-            document.getElementById("uf").innerHTML = response.uf;
+            console.log(response);
+            //document.getElementById("logradouro").innerHTML = response.logradouro;
+            //document.getElementById("cidade").innerHTML = response.localidade;
+            //document.getElementById("bairro").innerHTML = response.bairro;
+            //document.getElementById("uf").innerHTML = response.uf;
+            
+            //Alternative .ajax $("id").html(responde.value);
+            $("#logradouro").html(response.logradouro);
+            $("#cidade").html(response.localidade);
+            $("#bairro").html(response.bairro);
+            $("#uf").html(response.uf);
+            $("#cepTitle").html("CEP " + response.cep);
 
-            //Usando jquerry puro para consultar a UF
-            $("#ufAJ").html(response.uf);
         }
     })
 }
