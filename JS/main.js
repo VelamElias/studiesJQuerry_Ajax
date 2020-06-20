41dffa22ec551777e3c3a2fcaf6b3d64218a1eee
VelamElias <elnatasource@gmail.com>
2020-06-20 15:31:45 -0300

    include progress bar

--- JS/main.js
+++ JS/main.js
@@ -2,6 +2,9 @@
 
 function consultaCEP()
 {
+    //show a progress bar while the consult is running
+    $(".bar-progress").show();
+
     var cep = document.getElementById("cep").value;
     var url = "https://viacep.com.br/ws/" + cep +"/json/";
     
@@ -16,13 +19,24 @@ function consultaCEP()
             //document.getElementById("bairro").innerHTML = response.bairro;
             //document.getElementById("uf").innerHTML = response.uf;
             
-            //Alternative .ajax $("id").html(responde.value);
+            //Alternative .ajax $("#id").html(responde.value);
             $("#logradouro").html(response.logradouro);
             $("#cidade").html(response.localidade);
             $("#bairro").html(response.bairro);
             $("#uf").html(response.uf);
             $("#cepTitle").html("CEP " + response.cep);
 
+            //show the table with cep information
+            $(".cep").show();
+            //hide the progress bar when consult finish
+            $(".bar-progress").hide();
         }
     })
 }
+
+//hide the cep table until search button be press
+//jquerry
+$(function(){
+    $(".cep").hide();
+    $(".bar-progress").hide();
+});
